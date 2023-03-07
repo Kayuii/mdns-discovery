98d79902074f2376e1f92cb3d5c3e0af2ad32828
kayuii <577738@qq.com>
2023-03-07 13:25:20 +0800

    fix docker-entrypoint.sh args

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,22 @@
 set -e
 
 if [ $(echo "$1" | cut -c1) = "-" ]; then
-    echo "$0: assuming arguments for mdnscli"
+    echo "$0: assuming arguments for mdnscli service"
+    set -- mdnscli service "$@"
+fi
+
+if [ "$1" = "mdnscli"  ]; then
+    echo "$0: assuming arguments for mdnscli service"
+    set -- "$@"
+fi
+
+if [ "$1" = "service" ]; then
+    echo "$0: assuming arguments for mdnscli service"
+    set -- mdnscli "$@"
+fi
+
+if [ "$1" = "client" ]; then
+    echo "$0: assuming arguments for mdnscli client"
     set -- mdnscli "$@"
 fi
 
